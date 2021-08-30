d49d254f9f96801f0753906c4ffff77d61c87455
archwaq <49713077+archwaq@users.noreply.github.com>
2021-08-30 01:11:04 -0700

    finished

---
+++ Generics/GenericsIntro/VehicleLibrary/Bike.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//  3. Напишете си клас Bike и му задайте да има следните свойства: Brand, Model и Color.​
+//  Създайте някоклко инстанции пак от класа Bike и ги добавете в свързания списък LinkedList.​
+//  Обходете го и принтирайте подробна информация за колите.​
+
+namespace VehicleLibrary
+{
+    public class Bike
+    {
+        public Bike()
+        {
+
+        }
+
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ Generics/GenericsIntro/VehicleLibrary/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//  2.Напишете си клас Car и му задайте да има следните свойства: Brand, Model, RegisterNumber и SeatsNumber.​
+//  Направете си няколко инстанции от коли и ги добавете в списък от Коли. ​
+//  Обходете списъка и принтирайте подробна информация за колите.
+
+namespace VehicleLibrary
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string RegisterNumber { get; set; }
+        public byte SeastsNumber { get; set; }
+    }
+}
