94aef02bdd5242a771b61a85afecc370bd37bfe7
archwaq <49713077+archwaq@users.noreply.github.com>
2021-09-04 04:31:02 -0700

    SQL - banking

---
+++ SQL-Demos/BankSystem-EarlyAlpha/BankSystem/BankSystem.BisinessObject/BankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSystem.BisinessObject
+{
+    public class BankAccount
+    {
+        public int Id { get; set; }
+        public string Statuse { get; set; }
+        public string Iban { get; set; }
+        public decimal SumTotal { get; set; }
+        public bool IsItPaying { get; set; }
+        public Client Client { get; set; }
+    }
+}
---
+++ SQL-Demos/BankSystem-EarlyAlpha/BankSystem/BankSystem.BisinessObject/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankSystem.BisinessObject
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string FamilyName { get; set; }
+        public DateTime YearOfBirth { get; set; }
+    }
+}
