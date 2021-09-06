f19cd18b17b27c9e0082cfbdb961711b7a194e6a
archwaq <49713077+archwaq@users.noreply.github.com>
2021-09-06 10:49:55 -0700

    Add files via upload

---
+++ Serialization XML and JSON Formats/SerializationFormats/ObjectLibrary/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ObjectLibrary
+{
+    public class Student
+    {
+        public Student()
+        {
+
+        }
+
+        [XmlElement]
+        public Student[] Students;
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string BriefDescription { get; set; }
+        public double Evaluation { get; set; }
+
+        public void Save(string fileName)
+        {
+            using (var stream = new FileStream(fileName, FileMode.Create))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(Student));
+                xml.Serialize(stream, this);
+            }
+        }
+
+        public Student LoadFromFile(string fileName)
+        {
+            using (var stream = new FileStream(fileName, FileMode.Open))
+            {
+                var xml = new XmlSerializer(typeof(Student));
+                return (Student)xml.Deserialize(stream);
+            }
+        }
+    }
+}
